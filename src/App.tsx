b5b099841f1dfcbc23d4508b06390e0719f33c54
Dachi Natenadze <dachinatenadzeb@gmail.com>
2024-06-26 04:59:54 -0700

    added speaker info

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
 import Items from "./components/Items";
+import SpeakerZX9 from "./components/SpeakerCard";
 
 function App() {
   return (
     <>
       <Header />
       <Items />
+      <SpeakerZX9 />
     </>
   );
 }
---
+++ src/components/SpeakerCard.tsx
@@ -0,0 +1,24 @@
+import SpeakerPic from "/product-zx9-speaker/mobile/image-removebg-preview(38).png";
+export default function SpeakerCard() {
+  return (
+    <>
+      <main>
+        <div className="flex justify-center items-center mt-[120px]">
+          <div className="w-[327px] h-[600px] flex-shrink-0 rounded-[8px] bg-[#D87D4A] flex flex-col items-center px-[24px]">
+            <img src={SpeakerPic} alt="" className="max-sm:mt-[55px]" />
+            <h2 className="mt-[33px] mb-[24px] text-[#FFF] text-center font-[Manrope] text-[36px] not-italic font-bold leading-[40px] uppercase max-w-[200px]">
+              ZX9 SPEAKER
+            </h2>
+            <p className="mb-[24px] text-[#FFF] text-center font-[Manrope] text-[15px] not-italic font-normal leading-[25px] w-full">
+              Upgrade to premium speakers that are phenomenally built to deliver
+              truly remarkable sound.
+            </p>
+            <button className="w-[160px] h-[48px] flex-shrink-0 bg-[#000] text-[#FFF] font-[Manrope] text-[13px] not-italic font-bold leading-[normal] tracking-[1px] uppercase">
+              See Product
+            </button>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
