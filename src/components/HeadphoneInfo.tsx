5924d7b65dd6686241f474ae0a66ae207ac8311a
Dachi Natenadze <dachinatenadzeb@gmail.com>
2024-06-25 22:56:11 -0700

    add Info XX99 Mark II headphones

---
+++ src/components/HeadphoneInfo.tsx
@@ -0,0 +1,27 @@
+export default function HeadphoneInfo() {
+  return (
+    <>
+      <main>
+        <section>
+          <div>
+            <div className="flex flex-col justify-center items-center gap-4 max-sm:mt-[108px]">
+              <span className="text-[#FFF] text-center font-[Manrope] text-[14px] not-italic font-normal leading-[normal] tracking-[10px] uppercase opacity-50">
+                NEW PRODUCT
+              </span>
+              <h2 className="text-[#FFF] text-center font-[Manrope] text-[36px] not-italic font-bold leading-[40px] uppercase mb-[10px]">
+                XX99 Mark II HeadphoneS
+              </h2>
+              <p className="text-[#FFF] text-center font-[Manrope] w-[295px] text-[15px] not-italic font-normal leading-[25px] opacity-70  mb-[16px]">
+                Experience natural, lifelike audio and exceptional build quality
+                made for the passionate music enthusiast.
+              </p>
+              <button className="w-[160px] h-[48px] flex-shrink-0 bg-[#D87D4A]">
+                See Product
+              </button>
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}
